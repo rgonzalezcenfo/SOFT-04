396ed429dc0083269118c9d21a3bc3a4f06bb3f1
verbrannt-bob <172117560+verbrannt-bob@users.noreply.github.com>
2026-03-26 15:32:09 -0600

    Semana 10 Clase 1: inicio de la configuraacion de base de datos en proyecto banco

---
+++ Semana6/ProyectoBanco/src/gonzalez/roberto/script_tablas.sql
@@ -0,0 +1,43 @@
+CREATE DATABSE db_banco;
+
+USE DATABASE db_banco;
+
+CREATE TABLE t_cliente(
+    nombre_completo VARCHAR(50) NOT NULL,
+    cedula  VARCHAR(10) PRIMARY KEY,
+    fecha_nacimiento DATE NOT NULL,
+    ocupacion VARCHAR(25),
+    residencia VARCHAR(25),
+    contrasenia VARCHAR(25) NOT NULL
+)
+
+CREATE TABLE t_cuentas_ahorro(
+    id VARCHAR(10) PRIMARY KEY,
+    saldo DOUBLE NOT NULL,
+    CONSTRAINT chk_saldo_shoroo CHECK ( saldo >= 100 ),
+    porcentaje_interes DOUBLE NOT NULL,
+    CONSTRAINT chk_porcentaje_interes_ahorro CHECK(porcentaje_interes >= 0 AND poncetaje_interes <= 1),
+    cedula_cliente VARCHAR(10),
+    FOREIGN KEY (cedula_cliente) REFERENCES t_cliente(cedula)
+)
+
+CREATE TABLE t_cuentas_credito(
+    id VARCHAR(10) PRIMARY KEY,
+    saldo DOUBLE NOT NULL,
+    CONSTRAINT chk_saldo_shoroo CHECK ( saldo <= 0 ),
+    limite DOUBLE NOT NULL,
+    porcentaje_interes DOUBLE NOT NULL,
+    CONSTRAINT chk_porcentaje_interes_credito CHECK(porcentaje_interes >= 0 AND poncetaje_interes <= 1),
+    cedula_cliente VARCHAR(10),
+    FOREIGN KEY (cedula_cliente) REFERENCES t_cliente(cedula)
+)
+
+CREATE TABLE t_cuentas_debito(
+     id VARCHAR(10) PRIMARY KEY,
+     saldo DOUBLE NOT NULL,
+     CONSTRAINT chk_saldo_shoroo CHECK ( saldo >= 0 ),
+     porcentaje_interes DOUBLE NOT NULL,
+     CONSTRAINT chk_porcentaje_interes_debito CHECK(porcentaje_interes >= 0 AND poncetaje_interes <= 1),
+     cedula_cliente VARCHAR(10),
+     FOREIGN KEY (cedula_cliente) REFERENCES t_cliente(cedula)
+)
